85086e697becdf4fb29015d7ddde8fba7e18ee3a
Shepard16 <shepardmicho@gmail.com>
2024-11-18 20:33:03 +0100

    Inital commit

---
+++ src/components/canvas/Computers.jsx
@@ -0,0 +1,66 @@
+import { Suspense, useEffect, useRef, useState } from "react"
+import { Canvas } from "@react-three/fiber"
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei"
+
+import CanvasLoader from '../Loader'
+
+const Computers = () => {
+  const computer = useGLTF('./desktop_pc/scene.gltf') 
+  const spotLightRef = useRef();
+  console.log(computer);
+
+  useEffect(() => {
+    if (spotLightRef.current && computer.scene) {
+      spotLightRef.current.target.position.set(0, -3.35, -1.5);
+      spotLightRef.current.target.updateMatrixWorld();
+    }
+  }, [computer.scene])
+
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.15} 
+      groundColor="black" />
+      <pointLight intensity={7} />
+      <spotLight
+      ref={spotLightRef}
+      position={[-20, 100, 10]}
+      angle={0.12}
+      penumbra={1}
+      intensity={0}
+      distance={100}
+      color=""
+      castShadow
+      shadow-mapSize={[1024, 1024]}
+      />
+      <primitive 
+      object={computer.scene}
+      scale={0.75}
+      position={[0, -3.35, -1.5]}
+      rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
+  )
+}
+
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+    frameloop="demand"
+    shadows
+    camera={{ position: [20, 3, 5], fov: 25 }}
+    gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls  
+        enableZoom={false}
+        maxPolarAngle={Math.PI / 2}
+        minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+      <Preload all/>
+    </Canvas>
+  )
+}
+
+export default ComputersCanvas;
