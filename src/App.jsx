f771e57f5687959890ecf5975963c1b80dbbf08e
Shepard16 <shepardmicho@gmail.com>
2025-01-12 14:13:51 +0100

    Alot of changes to the project, fixed About, contact, work a bit css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from "react-router-dom"
 
-import {About, Contact, Experience, Feedbacks, 
-Hero, Navbar, Tech, Works, StarsCanvas} from './components'
+import {About, Contact, Experience, 
+Hero, Navbar, Tech, StarsCanvas} from './components'
 
 const App = () => {  
   return (
@@ -15,8 +15,6 @@ const App = () => {
    <About />
    <Experience />
    <Tech />
-   <Works/>
-   <Feedbacks />
    <div className="relative z-0" >
     <Contact />
     <StarsCanvas />
